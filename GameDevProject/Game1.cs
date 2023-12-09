2518abe2f377ef2b784df6d56af2fb8cad7881b5
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2023-12-09 17:28:52 +0100

    Change window size and resolution

--- GameDevProject/Game1.cs
+++ GameDevProject/Game1.cs
@@ -1,7 +1,6 @@
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using SharpDX.Direct3D9;
 
 namespace GameDevProject
 {
@@ -10,19 +9,24 @@ namespace GameDevProject
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D _texture;
-        private Rectangle _rectFrame;
+        private Rectangle _rectFrame, _actualScreenRectangle;
+        private RenderTarget2D _nativeRenderTarget;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // TODO: Add your initialization logic here 
+            _nativeRenderTarget = new RenderTarget2D(GraphicsDevice, 192, 108);
             _rectFrame = new Rectangle(1,0,14,20);
+            _actualScreenRectangle = new Rectangle(0,0,1280,720);
             base.Initialize();
         }
 
@@ -46,9 +50,14 @@ namespace GameDevProject
 
         protected override void Draw(GameTime gameTime)
         {
+            GraphicsDevice.SetRenderTarget(_nativeRenderTarget);
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
             _spriteBatch.Draw(_texture, new Vector2(0, 0), _rectFrame, Color.White);
+            _spriteBatch.End();
+            GraphicsDevice.SetRenderTarget(null);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(_nativeRenderTarget, _actualScreenRectangle, Color.White);
             _spriteBatch.End();
             base.Draw(gameTime);
         }
