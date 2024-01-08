8abda4fe078d6617634bedf46bcb9d8fd51b9f16
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-01-08 01:54:16 +0100

    Create Level1 class

---
+++ GameDevProject/Level/Level1.cs
@@ -0,0 +1,32 @@
+﻿using GameDevProject.GameObject.Block;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.Level
+{
+    internal class Level1
+    {
+        Texture2D texture;
+        public Level1(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("tileGrass");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int x = 0; x < 256; x += 32)
+            {
+                for(int y = 0; y < 144; y += 32)
+                {
+                    BlockFactory.CreateBlock("NORMAL", x, y, texture).Draw(spriteBatch);
+                }
+            }
+        }
+    }
+}
