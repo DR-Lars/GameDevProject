754cfbd844fb1515644fd4fd095fd059958aa769
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-01-08 03:10:50 +0100

    Add Hitbox class

---
+++ GameDevProject/GameObject/Hitbox.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.GameObject
+{
+    internal class Hitbox
+    {
+        public Rectangle rectangle;
+
+        private Rectangle originalRectangle;
+
+        public Hitbox(Rectangle rectangle)
+        {
+            this.rectangle = rectangle;
+            Update(Vector2.Zero);
+        }
+
+        public void Update(Vector2 position)
+        {
+            rectangle = new Rectangle((position + (originalRectangle.Location.ToVector2() - position)).ToPoint(), originalRectangle.Size.ToVector2().ToPoint());
+        }
+
+
+        public bool IsTouching(Hitbox hitbox)
+        {
+            return rectangle.Intersects(hitbox.rectangle);
+        }
+    }
+}
