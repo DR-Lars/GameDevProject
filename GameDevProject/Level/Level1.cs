a90454eb6bff1c091b42d974cab7b970e5b5f549
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-01-08 04:37:53 +0100

    Add boulder to level

--- GameDevProject/Level/Level1.cs
+++ GameDevProject/Level/Level1.cs
@@ -12,21 +12,25 @@ namespace GameDevProject.Level
 {
     internal class Level1
     {
-        Texture2D texture;
+        private ContentManager _content;
         public Level1(ContentManager content)
         {
-            texture = content.Load<Texture2D>("tileGrass");
+            _content = content;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            //Background
             for (int x = 0; x < 256; x += 32)
             {
                 for(int y = 0; y < 144; y += 32)
                 {
-                    BlockFactory.CreateBlock("NORMAL", x, y, texture).Draw(spriteBatch);
+                    BlockFactory.CreateBlock("NORMAL", x, y, _content.Load<Texture2D>("tileGrass")).Draw(spriteBatch);
                 }
             }
+
+            //Obstacle
+            BlockFactory.CreateBlock("BOULDER", 50, 30, _content.Load<Texture2D>("tileBoulder")).Draw(spriteBatch);
         }
     }
 }
