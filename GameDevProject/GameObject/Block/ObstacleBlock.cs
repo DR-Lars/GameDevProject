10a77c028943c2de5bc80adb102c32efeaef0c0f
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-01-08 03:07:59 +0100

    Add ObstacleBlock class

---
+++ GameDevProject/GameObject/Block/ObstacleBlock.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.GameObject.Block
+{
+    internal class ObstacleBlock : Block
+    {
+        public Hitbox hitbox;
+        public ObstacleBlock(int x, int y, Texture2D texture) : base(x, y, texture)
+        {
+            BoundingBox = new Rectangle(x, y, 32, 32);
+            Passable = true;
+            Color = Color.White;
+            hitbox = new Hitbox(BoundingBox);
+        }
+
+    }
+}
