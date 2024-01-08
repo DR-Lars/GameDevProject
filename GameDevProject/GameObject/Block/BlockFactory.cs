a0b3182d4ff0c769f58772657550561b3aae7375
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-01-08 01:20:38 +0100

    Add BlockFactory class

---
+++ GameDevProject/GameObject/Block/BlockFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.GameObject.Block
+{
+    internal class BlockFactory
+    {
+        public static Block CreateBlock(
+        string type, int x, int y, Texture2D texture)
+        {
+            Block newBlock = null;
+            type = type.ToUpper();
+            if (type == "NORMAL")
+            {
+                newBlock = new Block(x, y, texture);
+            }
+            if (type == "TRAP")
+            {
+                //newBlock = new TrapBlock(x, y, graphics);
+            }
+            if (type == "SLIME")
+            {
+                //newBlock = new SlimeBlock(x, y, graphics);
+            }
+            return newBlock;
+        }
+    }
+
+}
