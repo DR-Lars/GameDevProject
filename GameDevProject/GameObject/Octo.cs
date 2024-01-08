a590ae1f6a0cbbb3afe313c31a943961bc8be78e
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-01-08 22:44:44 +0100

    Create octo class

---
+++ GameDevProject/GameObject/Octo.cs
@@ -0,0 +1,79 @@
+﻿using GameDevProject.Animate;
+using GameDevProject.Input;
+using GameDevProject.Movement;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.GameObject
+{
+    internal class Octo : IGameObject, IMovable
+    {
+        private Texture2D _texture;
+        private Animation _animation;
+        private MovementManager _movementManager;
+        private Ninja _target;
+        public Vector2 prevPosition;
+        public Hitbox hitbox { get; set; }
+        public bool active { get; set; } = true;
+        public Vector2 Acceleration { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Speed { get; set; }
+        public IInputReader InputReader { get; set; }
+
+        public Octo(Texture2D texture, Ninja target)
+        {
+            _movementManager = new MovementManager();
+
+            _texture = texture;
+            _target = target;
+
+            _movementManager.target = _target;
+
+            _animation = new Animation(0);
+            _animation.SelectAnimation(texture.Width, texture.Height, 4, 4);
+
+            Position = new Vector2(200, 20);
+            Speed = new Vector2((float)0.2, (float)0.2);
+
+            CalculateHitbox();
+        }
+        public void Update(GameTime gameTime)
+        {
+            if (active)
+            {
+                Move(gameTime.ElapsedGameTime.TotalSeconds);
+                _animation.Update(gameTime);
+                hitbox.Update(Position);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (active)
+            {
+                spriteBatch.Draw(_texture, Position, _animation.currentFrame.SourceRectangle, Color.White);
+            }
+        }
+
+        private void Move(double totalseconds)
+        {
+            _movementManager.secondCounter += totalseconds;
+            int aniDir = _movementManager.AnimationDirection(this, 3);
+            if (aniDir != _animation.direction)
+            {
+                _animation = new Animation(_movementManager.AnimationDirection(this, 1));
+            }
+            _animation.SelectAnimation(_texture.Width, _texture.Height, 4, 4);
+        }
+
+        private void CalculateHitbox()
+        {
+            hitbox = new Hitbox(new Rectangle((int)Position.X, (int)Position.Y, 14, 14));
+        }
+    }
+}
